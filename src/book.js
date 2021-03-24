dcae13d0a9d2d125c4f1097e9039543ced0f9789
Dean Cook <deancook371@gmail.com>
2021-03-24 15:36:52 -0600

    Complete iteration 1 book exercises

--- src/book.js
+++ src/book.js
@@ -1,12 +1,74 @@
-function createTitle() {
-  
+function createTitle(initialTitle) {
+  var modifiedTitle = "The " + initialTitle;
+
+  return modifiedTitle;
+}
+
+function buildMainCharacter(charName, charAge, charPronouns) {
+  var character = {
+    name: charName,
+    age: charAge,
+    pronouns: charPronouns,
+  }
+
+  return character;
+}
+
+function saveReview(reviewText, arrayName) {
+  //for loop checks if review identical -- while i is less than length of array,
+  for (var i = 0; i < arrayName.length; i++) {
+    //if contents of array at i are identical to reviewtext,
+    if (arrayName[i] === reviewText) {
+      //exit function
+      return;
+    }
+  }
+
+  arrayName.push(reviewText);
+  return;
+}
+
+function calculatePageCount(title) {
+  var newTitle = title;
+
+  //if this specific use case / this specific title
+  if (title === "The Dragon in the Summer") {
+    //add another "the" to it
+    newTitle = createTitle(title);
+  }
+
+  var pageCount = 0;
+
+  //for loop, add 20 pages per character of title
+  for (var i = 0; i < newTitle.length; i++) {
+    pageCount += 20;
+  }
+
+  return pageCount;
+}
+
+function writeBook(bookTitle, mainCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+
+  return book;
+}
+
+function editBook(initialBook) {
+  initialBook.pageCount = initialBook.pageCount * .75;
+
+  return initialBook.pageCount;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
 }
