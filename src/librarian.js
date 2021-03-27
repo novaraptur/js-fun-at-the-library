02bda5c5c13fd1b41b8a20f79360376423c2e752
Dean Cook <deancook371@gmail.com>
2021-03-27 12:44:03 -0600

    Complete iteration 4 and librarian.js exercises

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,49 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
+
+  greetPatron(name) {
+    if (name === "Amy") {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  findBook(bookTitle) {
+    //refactoring idea-- more efficient way of finding genre?
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (this.library.shelves.fantasy[i].title === bookTitle) {
+        this.library.shelves.fantasy.splice(i, 1);
+        return `Yes, we have ${bookTitle}`;
+      }
+    }
+
+    for (var i = 0; i < this.library.shelves.fiction.length; i++) {
+      if (this.library.shelves.fiction[i].title === bookTitle) {
+        this.library.shelves.fiction.splice(i, 1);
+        return `Yes, we have ${bookTitle}`;
+      }
+    }
+
+    for (var i = 0; i < this.library.shelves.nonFiction.length; i++) {
+      if (this.library.shelves.nonFiction[i].title === bookTitle) {
+        this.library.shelves.nonFiction.splice(i, 1);
+        return `Yes, we have ${bookTitle}`;
+      }
+    }
+
+    return `Sorry, we do not have ${bookTitle}`;
+  }
+
+  calculateLateFee(daysLate) {
+    var lateFee = Math.ceil(0.25 * daysLate);
+
+    return lateFee;
+  }
+
 }
 
 module.exports = Librarian;
